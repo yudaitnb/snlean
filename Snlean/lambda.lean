8ca959a18c479a54c9851a9daa304fac097b3d68
yudaitnb <yudai.tnb@gmail.com>
2024-01-18 19:39:46 +0900

    tmp

--- Snlean/lambda.lean
+++ Snlean/lambda.lean
@@ -20,9 +20,9 @@ def type_env := List (Nat × type)
   deriving BEq
 
 inductive has_type : type_env → term → type → Prop
-| var_rule : ∀ (env : type_env) (x : Nat) (t : type), env.elem (x, t) → has_type env (var x) t
-| abs_rule : ∀ (env : type_env) (x : Nat) (t1 t2 : type) (e : term), has_type ((x, t1) :: env) e t2 → has_type env (abs t1 e) (type.arrow t1 t2)
-| app_rule : ∀ (env : type_env) (e1 e2 : term) (t2 t1 : type), has_type env e1 (type.arrow t2 t1) → has_type env e2 t2 → has_type env (app e1 e2) t1
+| var_rule : ∀ (gamma : type_env) (x : Nat) (t : type), gamma.elem (x, t) → has_type gamma (var x) t
+| abs_rule : ∀ (gamma : type_env) (x : Nat) (t1 t2 : type) (e : term), has_type ((x, t1) :: gamma) e t2 → has_type gamma (abs t1 e) (type.arrow t1 t2)
+| app_rule : ∀ (gamma : type_env) (e1 e2 : term) (t2 t1 : type), has_type gamma e1 (type.arrow t2 t1) → has_type gamma e2 t2 → has_type gamma (app e1 e2) t1
 open has_type
 
 inductive value : term → Prop
@@ -40,15 +40,15 @@ inductive eval : term → term → Prop
 | e_app_right : ∀ (v e2 e2' : term), value v → eval e2 e2' → eval (app v e2) (app v e2')
 open eval
 
-theorem env_var_rule_noemp : ∀ (x : Nat) (T : type) (env : type_env), has_type env (var x) T → env ≠ [] := by
-  intro x T env ht env_is_emp;
+theorem env_var_rule_noemp : ∀ (x : Nat) (T : type) (gamma : type_env), has_type gamma (var x) T → gamma ≠ [] := by
+  intro x T gamma ht env_is_emp;
   cases ht with
   | var_rule _ _ _ env_is_not_emp =>
     rw [env_is_emp] at env_is_not_emp
     contradiction
 
-theorem progress : ∀ (t : term) (T : type) (env : type_env), has_type env t T ∧ env = [] → value t ∨ (∃t' : term, eval t t') := by
-  intro t T env ht;
+theorem progress : ∀ (t : term) (T : type) (gamma : type_env), has_type gamma t T ∧ gamma = [] → value t ∨ (∃t' : term, eval t t') := by
+  intro t T gamma ht;
   have t_wt := And.left ht;
   have env_is_emp := And.right ht;
   induction t_wt with
@@ -58,15 +58,15 @@ theorem progress : ∀ (t : term) (T : type) (env : type_env), has_type env t T 
   | abs_rule =>
     apply Or.intro_left
     apply value.abs
-  | app_rule env e1 e2 T1 T2 pre1 pre2 ihe1 ihe2 =>
+  | app_rule gamma e1 e2 T1 T2 pre1 pre2 ihe1 ihe2 =>
     cases e1 with
     | var x =>
-      have env_is_not_emp := env_var_rule_noemp x (arrow T1 T2) env pre1
+      have env_is_not_emp := env_var_rule_noemp x (arrow T1 T2) gamma pre1
       contradiction
     | abs _ _ =>
       cases e2 with
       | var x =>
-        have env_is_not_emp := env_var_rule_noemp x T1 env pre2
+        have env_is_not_emp := env_var_rule_noemp x T1 gamma pre2
         contradiction
       | abs _ _ =>
         apply Or.intro_right
@@ -99,39 +99,33 @@ theorem progress : ∀ (t : term) (T : type) (env : type_env), has_type env t T 
           apply e_app_left
           apply h
 
-theorem preservation : ∀ (e e' : term) (t : type) (env : type_env), has_type env e t → eval e e' → has_type env e' t :=
+theorem subst_preservation : ∀ (s t : term) (x : Nat) (S T : type) (gamma : type_env),
+  has_type gamma s S → has_type (List.cons (x, S) gamma) t T → has_type gamma (subst t x s) T := by
   sorry
-  -- arrow.intro
-  -- induction ht
-  --   -- var_rule
-  --   _ x t env_elem
-  --   -- abs_rule
-  --   _ env x t1 t2 e ht ih
-  --   -- app_rule
-  --   _ env e1 e2 t1 t2 ht1 ht2 ih1 ih2,
-    -- -- var_rule ケース
-    -- -- 空の環境では変数は型付けできないので、このケースは発生しない
-    -- { contradiction },
 
-    -- -- abs_rule ケース
-    -- -- 抽象化された項は値である
-    -- { left, apply value.abs },
-
-    -- -- app_rule ケース
-    -- -- 関数適用のケースを考える
-    -- -- e1 が値の場合、e2 が値であるか簡約可能であるかを考える
-    -- -- e1 が値でない場合、e1 を簡約する
-    -- { cases ih1 with v1 e1',
-    --   -- e1 が値の場合
-    --   { cases ih2 with v2 e2',
-    --     -- e2 も値の場合
-    --     { right, existsi subst e2 0 v1, apply eval.e_app_abs, assumption },
-    --     -- e2 を簡約できる場合
-    --     { right, cases e2' with e2' eval_e2, existsi app v1 e2', apply eval.e_app_right, assumption, assumption } },
-    --   -- e1 を簡約できる場合
-    --   { right, cases e1' with e1' eval_e1, existsi app e1' e2, apply eval.e_app_left, assumption } }
-
--- def x abs type.base (var 0)
-
-
-#eval [1, 2].elem 3
+theorem preservation : ∀ (e e' : term) (t : type) (gamma : type_env),
+  has_type gamma e t → eval e e' → has_type gamma e' t := by
+  intro e e' t gamma ht ev;
+  induction ht with
+  | var_rule _ _ _ pre =>
+    contradiction
+  | abs_rule =>
+    contradiction
+  | app_rule gamma e1 e2 T1 T2 pre1 pre2 ih1 ih2 =>
+    cases ev with
+    | e_app_abs T e v vv =>
+      cases vv with
+      | abs T b =>
+        apply subst_preservation e b T T1
+    | e_app_left e1 e1' e2' ev1 =>
+      apply has_type.app_rule
+      apply ihe1
+      assumption
+      assumption
+      assumption
+    | e_app_right v e2 e2' vv ev2 =>
+      apply has_type.app_rule
+      assumption
+      apply ihe2
+      assumption
+      assumption
